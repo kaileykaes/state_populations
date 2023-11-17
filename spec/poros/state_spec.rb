c94ce6aff64f4fc5d4a2f3f3349aeca665410ff7
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-11-17 13:30:36 -0700

    test state poro

---
+++ spec/poros/state_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+RSpec.describe State do
+  before(:each) do
+    states = CSV.open('spec/fixtures/state_pops_by_year.csv', headers: true, header_converters: :symbol)
+    state_info = states.first
+    @state = State.new(state_info)
+
+  end
+
+  describe 'initialize' do 
+    it 'exists and has attributes' do 
+      expect(@state).to be_a State
+      expect(@state.id).to eq('04000US01')
+      expect(@state.name).to eq('Alabama')
+      expect(@state.year_pops).to be_a Hash
+      expect(@state.year_pops).to eq({
+        '2013': 4799277
+      })
+    end
+  end
+end
