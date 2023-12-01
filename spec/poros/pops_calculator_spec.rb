bc16975a1ff88f76c320c630e12927f53b7b0574
Kailey Kaes <kaileykaes@gmail.com>
2023-12-01 13:40:02 -0700

    fix cli class tests to use fixtures

--- spec/poros/pops_calculator_spec.rb
+++ spec/poros/pops_calculator_spec.rb
@@ -1,34 +1,34 @@
 require 'spec_helper'
 
 RSpec.describe PopsCalulator do
-  before(:each) do
-    @pc = PopsCalulator.new('spec/fixtures/all_state_pops.csv')
-  end
-
   describe 'initialize' do 
     it 'exists and has attributes' do 
-      expect(@pc).to be_a PopsCalulator
-      expect(@pc.states).to be_a Array
-      expect(@pc.states.first).to be_a State
+      pc = PopsCalulator.new('spec/fixtures/all_state_pops.csv')
+      expect(pc).to be_a PopsCalulator
+      expect(pc.states).to be_a Array
+      expect(pc.states.first).to be_a State
     end
   end
 
   describe 'methods' do 
     it '#greatest_increase' do 
-      expect(@pc.greatest_increase).to eq('Puerto Rico')
+      pc = PopsCalulator.new('spec/fixtures/all_state_pops_washington_greatest_increase.csv')
+      expect(pc.greatest_increase).to eq('Washington')
     end
-
+    
     it '#greatest_decrease' do 
-      expect(@pc.greatest_decrease).to eq('Texas')
+      pc = PopsCalulator.new('spec/fixtures/all_state_pops_florida_greatest_decrease.csv')
+      expect(pc.greatest_decrease).to eq('Florida')
     end
-
+    
     it '#lowest_increase' do 
-      @pc.lowest_increase
-      expect(@pc.lowest_increase).to eq('Mississippi')
+      pc = PopsCalulator.new('spec/fixtures/all_state_pops_dc_lowest_increase.csv')
+      expect(pc.lowest_increase).to eq('District of Columbia')
     end
-
+    
     it '#lowest_decrease' do 
-      expect(@pc.lowest_decrease).to eq('Wyoming')
+      pc = PopsCalulator.new('spec/fixtures/all_state_pops_vermont_lowest_decrease.csv')
+      expect(pc.lowest_decrease).to eq('Vermont')
     end
   end
 end
